33a9d92dd6b1c734427b142c5edcbe50a61cbcf0
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2022-12-20 00:40:17 +0200

    centred loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,21 @@
 import { Oval } from 'react-loader-spinner';
+import { Box } from 'components/Box';
 
 export default function Loader() {
   return (
-    <Oval
-      height={80}
-      width={80}
-      color="#4fa94d"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="oval-loading"
-      secondaryColor="#4fa94d"
-      strokeWidth={2}
-      strokeWidthSecondary={2}
-    />
+    <Box display="flex" justifyContent="center" alignItems="center" py={5}>
+      <Oval
+        height={80}
+        width={80}
+        color="teal"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="teal"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
+    </Box>
   );
 }
